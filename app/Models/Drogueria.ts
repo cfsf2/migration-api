4974a787d483ccba450083cb7523745e6d6cfe21
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 11:02:34 -0300

    Model Drogueria

---
+++ app/Models/Drogueria.ts
@@ -0,0 +1,37 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Usuario from "./Usuario";
+
+export default class Drogueria extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public habilitado: string;
+
+  @column()
+  public id_usuario_creacion: number;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column()
+  public id_usuario_modificacion: number;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  //foreing keys
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public drogueria_id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public drogueria_id_usuario_modificacion: HasOne<typeof Usuario>;
+}
