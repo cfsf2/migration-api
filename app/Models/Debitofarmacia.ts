f397158fab943b14772e4b485c11a8c54d94599d
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 16:43:14 -0300

    Model DebitoFarmacia

---
+++ app/Models/Debitofarmacia.ts
@@ -0,0 +1,35 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Usuario from "./Usuario";
+
+export default class PedidoProductoPack extends BaseModel {
+  public static table = "tbl_debitofarmacia";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public usuario: string;
+
+  @column()
+  public periodo: string;
+
+  @column()
+  public archivo: string;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_modificacion: HasOne<typeof Usuario>;
+}
