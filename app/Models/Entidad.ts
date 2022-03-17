dd4edef668d0f07ead8934b3ab6bd1be24c37cec
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 12:40:57 -0300

    Model Entidad

---
+++ app/Models/Entidad.ts
@@ -0,0 +1,48 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Usuario from "./Usuario";
+
+export default class Entidad extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public habilitado: string;
+
+  @column()
+  public imagen: string;
+
+  @column()
+  public logo: string;
+
+  @column()
+  public titulo: string;
+
+  @column()
+  public email: string;
+
+  @column()
+  public rentabilidad: string;
+
+  @column()
+  public mostrar_en_proveeduria: string;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_modificacion: HasOne<typeof Usuario>;
+}
