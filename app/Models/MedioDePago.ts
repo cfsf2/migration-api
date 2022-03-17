423935442de745e846c9477f48fd3ad63e5ce68e
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 11:31:19 -0300

    Model Medio De Pago

---
+++ app/Models/MedioDePago.ts
@@ -0,0 +1,36 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Usuario from "./Usuario";
+
+export default class MedioDePago extends BaseModel {
+  public static table = "tbl_mediodepago";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public id_usuario_creacion: number;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column()
+  public id_usuario_modificacion: number;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  //foreing key
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public mediodepago_id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public mediodepago_id_usuario_modificacion: HasOne<typeof Usuario>;
+}
