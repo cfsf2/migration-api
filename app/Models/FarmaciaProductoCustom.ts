66f0c666b9610dba6b16c0666abab4e0c2778ad0
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 12:51:26 -0300

    Model FarmaciaProductoCustom

--- app/Models/FarmaciaProductoCustom.ts
+++ app/Models/FarmaciaProductoCustom.ts
@@ -1,42 +1,33 @@
 import { DateTime } from "luxon";
 import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Farmacia from "./Farmacia";
 import Usuario from "./Usuario";
 
-export default class Entidad extends BaseModel {
-  public static table = "tbl_entidad";
+export default class FarmaciaProductoCustom extends BaseModel {
+  public static table = "tbl_farmacia_producto_custom";
 
   @column({ isPrimary: true })
   public id: number;
 
   @column()
-  public nombre: string;
-
-  @column()
   public habilitado: string;
-
-  @column()
-  public imagen: string;
-
-  @column()
-  public logo: string;
-
-  @column()
-  public titulo: string;
-
-  @column()
-  public email: string;
-
-  @column()
-  public rentabilidad: string;
-
-  @column()
-  public mostrar_en_proveeduria: string;
 
   @column.dateTime({ autoCreate: true })
   public ts_creacion: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public ts_modificacion: DateTime;
+
+  //foreing key
+  @hasOne(() => Farmacia, {
+    foreignKey: "id",
+  })
+  public id_farmacia: HasOne<typeof Farmacia>;
+
+  @hasOne(() => ProductoCustom, {
+    foreignKey: "id",
+  })
+  public id_producto_custom: HasOne<typeof ProductoCustom>;
 
   @hasOne(() => Usuario, {
     foreignKey: "id",
