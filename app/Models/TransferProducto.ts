343ed78dd95b3357672a38ba38c45f14b744bd96
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 17:00:44 -0300

    Model TransferProducto

---
+++ app/Models/TransferProducto.ts
@@ -0,0 +1,58 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Usuario from "./Usuario";
+
+export default class PedidoProductoPack extends BaseModel {
+  public static table = "tbl_transfer_producto";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public id_laboratorio: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public habilitado: string;
+
+  @column()
+  public presentacion: string;
+
+  @column()
+  public cantidad_minima: number;
+
+  @column()
+  public descuento_porcentaje: number;
+
+  @column()
+  public precio: number;
+
+  @column()
+  public codigo: string;
+
+  @column()
+  public en_papelera: string;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_modificacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Laboratorio, {
+    foreignKey: "id",
+  })
+  public id_laboratorio: HasOne<typeof Laboratorio>;
+}
