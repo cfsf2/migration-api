e1f0c175be73a9ada530b7ec6a2fa552b329bcee
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 11:55:03 -0300

    Model SolicitudProveeduria

---
+++ app/Models/SolicitudProveeduria.ts
@@ -0,0 +1,62 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Farmacia from "./Farmacia";
+import Usuario from "./Usuario";
+
+export default class SolicitudProveeduria extends BaseModel {
+  public static table = "tbl_solicitud_proveeduria";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public id_farmacia: number;
+
+  @column()
+  public id_entidad: number;
+
+  @column.dateTime({ autoCreate: true })
+  public fecha: DateTime;
+
+  @column()
+  public nro_cuenta_drogueria: string;
+
+  @column()
+  public email_destinatario: string;
+
+  @column()
+  public productos_solicitados: string;
+
+  @column()
+  public id_usuario_creacion: number;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column()
+  public id_usuario_modificacion: number;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  //foreing key
+  @hasOne(() => Entidad, {
+    foreignKey: "id",
+  })
+  public solicitud_proveeduria_id_entidad: HasOne<typeof Entidad>;
+
+  @hasOne(() => Farmacia, {
+    foreignKey: "id",
+  })
+  public solicitud_proveeduria_id_farmacia: HasOne<typeof Farmacia>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public solicitud_proveeduria_id_usuario_modificacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public solicitud_proveeduria_id_usuario_creacion: HasOne<typeof Usuario>;
+}
