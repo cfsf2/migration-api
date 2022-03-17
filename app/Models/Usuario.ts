ba0f0322358792efa5f3024fa063f3ad1010a197
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-17 12:23:41 -0300

    Model _counter

--- app/Models/Usuario.ts
+++ app/Models/Usuario.ts
@@ -1,12 +1,11 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Usuario extends BaseModel {
-
-  public static table = 'tbl_usuario';
+  public static table = "tbl_usuario";
 
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
   public usuario: string;
@@ -21,10 +20,7 @@ export default class Usuario extends BaseModel {
   public dni: Number;
 
   @column()
-  public fecha_nac: Date
-
-  @column()
-  public id_localidad: number
+  public fecha_nac: Date;
 
   @column()
   public email: string;
@@ -33,47 +29,56 @@ export default class Usuario extends BaseModel {
   public password: string;
 
   @column()
-  public newsletter: string
+  public newsletter: string;
 
   @column()
-  public habilitado: string
+  public habilitado: string;
 
   @column()
-  public esfarmacia: string
+  public esfarmacia: string;
 
   @column()
-  public admin: string
+  public admin: string;
 
   @column()
-  public confirmado: string
+  public confirmado: string;
 
   @column()
-  public telefono: string
+  public telefono: string;
 
   @column.dateTime()
-  public f_ultimo_acceso: DateTime
+  public f_ultimo_acceso: DateTime;
 
   @column()
-  public deleted: string
+  public deleted: string;
 
   @column()
-  public demolab: string
+  public demolab: string;
 
   @column()
-  public id_wp: string
-
-  @column()
-  public id_usuario_creacion: number
+  public id_wp: string;
 
   @column.dateTime({ autoCreate: true })
-  public ts_creacion: DateTime
-
-  @column()
-  public id_usuario_modificacion: number
+  public ts_creacion: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public ts_modificacion: DateTime
+  public ts_modificacion: DateTime;
 
   @column()
-  public celular: string
+  public celular: string;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Localidad, {
+    foreignKey: "id",
+  })
+  public id_localidad: HasOne<typeof Localidad>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_modificacion: HasOne<typeof Usuario>;
 }
