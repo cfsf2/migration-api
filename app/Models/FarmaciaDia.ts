e6c1b37774cad3e32c2dca56ce30905156d1c0ae
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-18 10:04:52 -0300

    Model FarmaciaDia

---
+++ app/Models/FarmaciaDia.ts
@@ -0,0 +1,46 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Farmacia from "./Farmacia";
+import Usuario from "./Usuario";
+
+export default class PedidoProductoPack extends BaseModel {
+  public static table = "tbl_farmacia_dia";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public inicio: string;
+
+  @column()
+  public fin: string;
+
+  @column()
+  public habilitado: string;
+
+  @column.dateTime({ autoCreate: true })
+  public ts_creacion: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public ts_modificacion: DateTime;
+
+  @hasOne(() => Farmacia, {
+    foreignKey: "id",
+  })
+  public id_farmacia: HasOne<typeof Farmacia>;
+
+  @hasOne(() => Dia, {
+    foreignKey: "id",
+  })
+  public id_dia: HasOne<typeof Dia>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_creacion: HasOne<typeof Usuario>;
+
+  @hasOne(() => Usuario, {
+    foreignKey: "id",
+  })
+  public id_usuario_modificacion: HasOne<typeof Usuario>;
+}
