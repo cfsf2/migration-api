4e06c9f433ba21c73d05477c6fb6a2ab7c02c34c
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-18 09:44:22 -0300

    Model SConf

---
+++ app/Models/SConf.ts
@@ -0,0 +1,23 @@
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import STipo from "./STipo";
+
+export default class PedidoProductoPack extends BaseModel {
+  public static table = "s_conf";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public permiso: string;
+
+  @column()
+  public id_a: string;
+
+  @hasOne(() => STipo, {
+    foreignKey: "id",
+  })
+  public id_tipo: HasOne<typeof STipo>;
+}
