82491503562a3ae5c98b87ca5f58dd708c9bd084
Bruno Ramari <bjramariutn@hotmail.com>
2022-03-18 15:33:19 -0300

    conexion sql

--- migracion-scripts/scripts/mysqlCon.js
+++ migracion-scripts/scripts/mysqlCon.js
@@ -1,19 +1,19 @@
 var mysql = require("mysql");
 
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "apifarmageotest_migracion",
-//   password: "migracion_2309",
-//   database: "apifarmageotest_bd",
-//   charset: "utf8mb4",
-// });
 const con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "pass",
-  database: "farmageo",
+  user: "apifarmageotest_migracion",
+  password: "migracion_2309",
+  database: "apifarmageotest_bd",
   charset: "utf8mb4",
 });
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "pass",
+//   database: "farmageo",
+//   charset: "utf8mb4",
+// });
 con.query("SET GLOBAL connect_timeout=28800");
 con.query("SET GLOBAL interactive_timeout=28800");
 con.query("SET GLOBAL wait_timeout=28800");
