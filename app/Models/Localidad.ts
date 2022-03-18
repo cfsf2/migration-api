c2f7b54ab8654e90825a473bd8480ad7a3df3dc3
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-18 11:33:35 -0300

    Model Localidad

---
+++ app/Models/Localidad.ts
@@ -0,0 +1,27 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import Departamento from "./Departamento";
+
+export default class Localidad extends BaseModel {
+  public static table = "tbl_localidad";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public nombre: string;
+
+  @column()
+  public centroide_lon: string;
+
+  @column()
+  public centroide_lat: string;
+
+  @column()
+  public cp: number;
+
+  @hasOne(() => Departamento, {
+    foreignKey: "id",
+  })
+  public id_departamento: HasOne<typeof Departamento>;
+}
