9b483a4edb81dd92bdd2ee333a6634e61361abd9
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-03-18 10:57:34 -0300

    Model SConfCpsc

---
+++ app/Models/SConfCpsc.ts
@@ -0,0 +1,32 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, hasOne, HasOne } from "@ioc:Adonis/Lucid/Orm";
+import SConf from "./SConf";
+
+export default class SConfCpsc extends BaseModel {
+  public static table = "s_conf_cpsc";
+
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public orden: number;
+
+  @column()
+  public condicion: string;
+
+  @column()
+  public evaluar: string;
+
+  @column()
+  public sql: string;
+
+  @hasOne(() => SConf, {
+    foreignKey: "id",
+  })
+  public id_conf: HasOne<typeof SConf>;
+
+  @hasOne(() => SConf, {
+    foreignKey: "id",
+  })
+  public id_conf_h: HasOne<typeof SConf>;
+}
