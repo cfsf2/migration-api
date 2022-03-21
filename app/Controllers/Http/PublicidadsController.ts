e215fe2021745acdf0cb976b351cf02e4978357c
Bruno Ramari <bjramariutn@hotmail.com>
2022-03-21 16:28:17 -0300

    model

--- app/Controllers/Http/PublicidadsController.ts
+++ app/Controllers/Http/PublicidadsController.ts
@@ -4,33 +4,11 @@ import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class PublicidadsController {
   public async index({ request }: HttpContextContract) {
-    const query = await Database.from("tbl_publicidad as p")
-      .select(
-        "p.*",
-        "tp.nombre as tipo",
-        "cp.nombre as color",
-        Database.raw("GROUP_CONCAT(i.id) as instituciones")
-      )
-      .leftJoin(
-        "tbl_publicidad_tipo as tp",
-        "tp.id",
-        "=",
-        "p.id_publicidad_tipo"
-      )
-      .leftJoin("tbl_publicidad_color as cp", "cp.id", "=", "p.id_color")
-      .leftJoin(
-        "tbl_publicidad_institucion as ip",
-        "ip.id_publicidad",
-        "=",
-        "p.id"
-      )
-      .leftJoin("tbl_institucion as i", "ip.id_institucion", "=", "i.id")
-      .groupBy("p.id");
-
-    return await Publicidad.query()
-      .preload("instituciones")
-      .preload("tipo")
-      .select("*")
-      .where("id_publicidad_tipo", "=", "1");
+    return Publicidad.traerPublicidades();
+    // return await Publicidad.query()
+    //   .preload("instituciones")
+    //   .preload("juancito")
+    //   .select("*")
+    //   .where("id_publicidad_tipo", "=", "1");
   }
 }
--- app/Models/Publicidad.ts
+++ app/Models/Publicidad.ts
@@ -14,8 +14,33 @@ import PublicidadColor from "./PublicidadColor";
 import PublicidadTipo from "./PublicidadTipo";
 import Institucion from "./Institucion";
 import PublicidadInstitucion from "./PublicidadInstitucion";
+import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class Publicidad extends BaseModel {
+  static traerPublicidades() {
+    const query = Database.from("tbl_publicidad as p")
+      .select(
+        "p.*",
+        "tp.nombre as tipo",
+        "cp.nombre as color",
+        Database.raw("GROUP_CONCAT(i.id) as instituciones")
+      )
+      .leftJoin(
+        "tbl_publicidad_tipo as tp",
+        "tp.id",
+        "=",
+        "p.id_publicidad_tipo"
+      )
+      .leftJoin("tbl_publicidad_color as cp", "cp.id", "=", "p.id_color")
+      .leftJoin(
+        "tbl_publicidad_institucion as ip",
+        "ip.id_publicidad",
+        "=",
+        "p.id"
+      )
+      .leftJoin("tbl_institucion as i", "ip.id_institucion", "=", "i.id")
+      .groupBy("p.id");
+  }
   public static table = "tbl_publicidad";
 
   @column({ isPrimary: true })
@@ -45,11 +70,17 @@ export default class Publicidad extends BaseModel {
   @column()
   public id_publicidad_tipo: Number;
 
+  @column()
+  public id_publicidad_color: Number;
+
   @column.dateTime({ autoCreate: true })
   public ts_creacion: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public ts_modificacion: DateTime;
+
+  @column()
+  public id_usuario_modificacion: Number;
 
   @hasOne(() => Usuario, {
     foreignKey: "id",
@@ -59,25 +90,26 @@ export default class Publicidad extends BaseModel {
   @hasOne(() => Usuario, {
     foreignKey: "id",
   })
-  public id_usuario_modificacion: HasOne<typeof Usuario>;
+  public usuario_modificacion: HasOne<typeof Usuario>;
 
   @hasOne(() => PublicidadTipo, {
     foreignKey: "id",
     localKey: "id_publicidad_tipo",
   })
-  public tipo: HasOne<typeof PublicidadTipo>;
+  public juancito: HasOne<typeof PublicidadTipo>;
 
   @hasOne(() => PublicidadColor, {
     foreignKey: "id",
+    localKey: "id_publicidad_color",
   })
   public color: HasOne<typeof PublicidadColor>;
 
   @hasManyThrough([() => Institucion, () => PublicidadInstitucion], {
-    foreignKey: "id_publicidad",
-    localKey: "id",
-    throughForeignKey: "id",
-    throughLocalKey: "id_institucion",
-    serializeAs: "Institucion_",
+    localKey: "id", // tbl_publicidad
+    foreignKey: "id_publicidad", // tbl_publicidad_institucion
+    throughLocalKey: "id_institucion", // tbl_publicidad_institucion
+    throughForeignKey: "id", // tbl_institucion
+    // serializeAs: "Instituciones",
   })
   public instituciones: HasManyThrough<typeof Institucion>;
 }
