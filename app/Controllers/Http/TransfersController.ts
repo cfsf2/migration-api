5ec90451ef54177c08eaf484c289bbdfbd7b3a28
Bruno Ramari <bjramari@hotmail.com>
2022-04-21 15:55:32 -0300

    transfers:

--- app/Controllers/Http/TransfersController.ts
+++ app/Controllers/Http/TransfersController.ts
@@ -3,6 +3,10 @@ import Transfer from "App/Models/Transfer";
 
 export default class TransfersController {
   public async mig_index({ request }: HttpContextContract) {
-    return await Transfer.traerTransfers()
+    return await Transfer.traerTransfers();
+  }
+
+  public async mig_add({ request }: HttpContextContract) {
+    Transfer.guardar(request.body());
   }
 }
