3d1bd5ca225b7d5c2f3a684a13fef5b8a59918a9
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-05-02 10:31:25 -0300

    corrección transfer

--- app/Controllers/Http/TransfersController.ts
+++ app/Controllers/Http/TransfersController.ts
@@ -4,18 +4,18 @@ import Transfer from "App/Models/Transfer";
 
 export default class TransfersController {
   public async mig_index({ bouncer }: HttpContextContract) {
-    await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_GET);
+    //await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_GET);
 
     return await Transfer.traerTransfers({});
   }
 
   public async mig_byFarmacia({ request, bouncer }: HttpContextContract) {
-    await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_GET_FARMACIA);
+    //await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_GET_FARMACIA);
     return await Transfer.traerTransfers({ id_farmacia: request.params().id });
   }
 
   public async mig_add({ request, auth, bouncer }: HttpContextContract) {
-    await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_CREATE);
+    //await bouncer.authorize("AccesoRuta", Permiso.TRANSFER_CREATE);
     const usuario = auth.user;
 
     return await Transfer.guardar({ data: request.body(), usuario: usuario });
