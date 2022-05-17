90fc3addef6a4ff46f6a912a1cde08546443511f
Bruno Ramari <bjramari@hotmail.com>
2022-05-17 16:28:07 -0300

    listado

--- app/Controllers/Http/ConfigsController.ts
+++ app/Controllers/Http/ConfigsController.ts
@@ -17,6 +17,11 @@ export default class ConfigsController {
   public async Config({ request }) {
     const config = request.qs().pantalla;
 
+    console.log(config);
+    if (!config) {
+      console.log({ datos: [], cabeceras: [], filtros: [] });
+      return { datos: [], cabeceras: [], filtros: [] };
+    }
     const conf = await SConf.query()
       .where("id_a", config)
       .andWhere("id_tipo", 1)
@@ -26,22 +31,27 @@ export default class ConfigsController {
       .preload("sub_conf", (query) => preloadRecursivo(query))
       .firstOrFail();
 
-    const modelo = conf.sub_conf
-      .find((sc) => sc.tipo.id === 2)
-      ?.valores.find((val) => val.atributo.find((a) => a.id === 15))
+    // para listado
+    const listado = conf.sub_conf.find((sc) => sc.tipo.id === 2);
+    console.log(conf?.toJSON());
+
+    let opcionesListado = {};
+
+    listado?.valores.forEach((val) => {
+      opcionesListado[val.atributo[0].nombre] = val.valor;
+    });
+
+    const modelo = listado?.valores
+      .find((val) => val.atributo.find((a) => a.id === 15))
       ?.toObject().valor;
 
-    const columnas = conf.sub_conf
-      .find((sc) => sc.tipo.id === 2)
-      ?.sub_conf.map((sc) => {
-        if (sc.tipo.id === 4) {
-          return sc;
-        }
-      });
+    const columnas = listado?.sub_conf.filter((sc) => sc.tipo.id === 4);
 
-    const campos = columnas?.map((col) => {
-      return col?.valores.find((v) => v.atributo[0].id === 7)?.valor;
-    });
+    const campos = columnas
+      ?.map((col) => {
+        return col?.valores.find((v) => v.atributo[0].id === 7)?.valor;
+      })
+      .filter((c) => c);
 
     const cabeceras = columnas?.map((col) => {
       let cabecera = {};
@@ -52,7 +62,21 @@ export default class ConfigsController {
       return cabecera;
     });
 
+    const filtros_aplicables = listado?.sub_conf.filter(
+      (sc) => sc.tipo.id === 3
+    );
+
+    const filtros = filtros_aplicables?.map((fil) => {
+      let filters = {};
+
+      fil?.valores.forEach((val) => {
+        filters[val.atributo[0].nombre] = val.valor;
+      });
+      return filters;
+    });
+
     const datos = await eval(modelo).query().select(campos);
-    return { datos, cabeceras };
+    // console.log(conf.toJSON());
+    return { datos, cabeceras, filtros, opcionesListado };
   }
 }
