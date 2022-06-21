5674be8408fb2e53fe0e2adc1f49ae426655e5fd
Bruno Ramari <bjramari@hotmail.com>
2022-06-21 13:28:31 -0300

    ruta update

--- routes/pantalla.ts
+++ routes/pantalla.ts
@@ -3,3 +3,5 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.post("config/", "ConfigsController.Config");
 
 Route.post("pantalla/:pantalla", "ConfigsController.ConfigPantalla");
+
+Route.post("guardar", "ConfigController.Update");
