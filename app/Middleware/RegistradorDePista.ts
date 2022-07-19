a4019fe2034cdd2cdf6aaacfe7a5fd9d18eb2b0a
Bruno Ramari <bjramari@hotmail.com>
2022-07-19 10:41:01 -0300

    registrador solo si hay usuario

--- app/Middleware/RegistradorDePista.ts
+++ app/Middleware/RegistradorDePista.ts
@@ -2,14 +2,16 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class RegistradorDePista {
   public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
-    const acceso = {
-      usuario: ctx.usuario?.id,
-      usuario_nombre: ctx.usuario?.nombre,
-      url: ctx.request.url(),
-      pantalla: ctx.request.param("pantalla"),
-      filtros: ctx.request.qs(),
-    };
-    console.log(acceso);
+    if (ctx.usuario.id) {
+      const acceso = {
+        usuario: ctx.usuario?.id,
+        usuario_nombre: ctx.usuario?.nombre,
+        url: ctx.request.url(),
+        pantalla: ctx.request.param("pantalla"),
+        filtros: ctx.request.qs(),
+      };
+      console.log(acceso);
+    }
     await next();
   }
 }
