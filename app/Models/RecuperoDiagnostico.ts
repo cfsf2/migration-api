1a72ac6d19692cd722b1d9ae40e3d729e10ea789
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-08-17 16:13:48 -0300

    forek

--- app/Models/RecuperoDiagnostico.ts
+++ app/Models/RecuperoDiagnostico.ts
@@ -1,5 +1,6 @@
 import { DateTime } from "luxon";
-import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { BaseModel, column, HasOne, hasOne } from "@ioc:Adonis/Lucid/Orm";
+import Diagnostico from "./Diagnostico";
 
 export default class RecuperoDiagnostico extends BaseModel {
   public static table = "tbl_recupero_dignosticos";
@@ -7,11 +8,20 @@ export default class RecuperoDiagnostico extends BaseModel {
   @column({ isPrimary: true })
   public id: number;
 
+  @column()
+  public id_diagnostico: string;
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime;
+
+  @hasOne(() => Diagnostico, {
+    foreignKey: "id",
+    localKey: "id_diagnostico",
+  })
+  public diagnostico: HasOne<typeof Diagnostico>;
 
   public serializeExtras() {
     const keys = Object.keys(this.$extras);
--- app/Models/RecuperoEstadio.ts
+++ app/Models/RecuperoEstadio.ts
@@ -1,13 +1,35 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column, HasOne, hasOne } from "@ioc:Adonis/Lucid/Orm";
+import Estadio from "./Estadio";
 
 export default class RecuperoEstadio extends BaseModel {
+  public static table = "tbl_recupero_estadio";
+
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
+
+  @column()
+  public id_estadio: number;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
+
+  @hasOne(() => Estadio, {
+    foreignKey: "id",
+    localKey: "id_estadio",
+  })
+  public estadio: HasOne<typeof Estadio>;
+
+  public serializeExtras() {
+    const keys = Object.keys(this.$extras);
+    const extras = {};
+    keys.forEach((k) => {
+      if (k === "_id") return (extras[k] = this.$extras[k].toString());
+      extras[k] = this.$extras[k];
+    });
+    return extras;
+  }
 }
