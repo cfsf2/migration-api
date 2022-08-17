11b64dd73167163f48baf6b4ae5329047d4cfcc2
Bruno Ramari <bjramari@hotmail.com>
2022-08-17 15:30:14 -0300

    modelos

--- app/Models/Diagnostico.ts
+++ app/Models/Diagnostico.ts
@@ -1,4 +1,3 @@
-import { DateTime } from "luxon";
 import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Diagnostico extends BaseModel {
@@ -13,9 +12,13 @@ export default class Diagnostico extends BaseModel {
   @column()
   public nombre: string;
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+  public serializeExtras() {
+    const keys = Object.keys(this.$extras);
+    const extras = {};
+    keys.forEach((k) => {
+      if (k === "_id") return (extras[k] = this.$extras[k].toString());
+      extras[k] = this.$extras[k];
+    });
+    return extras;
+  }
 }
--- app/Models/Monodro.ts
+++ app/Models/Monodro.ts
@@ -18,4 +18,14 @@ export default class Monodro extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updated_at: DateTime;
+
+  public serializeExtras() {
+    const keys = Object.keys(this.$extras);
+    const extras = {};
+    keys.forEach((k) => {
+      if (k === "_id") return (extras[k] = this.$extras[k].toString());
+      extras[k] = this.$extras[k];
+    });
+    return extras;
+  }
 }
