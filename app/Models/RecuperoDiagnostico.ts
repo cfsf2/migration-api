9b4956cad944e0c396a1b2b6d0036df267513618
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-08-17 15:34:21 -0300

    serilas recuperodiagnostico

--- app/Models/RecuperoDiagnostico.ts
+++ app/Models/RecuperoDiagnostico.ts
@@ -1,15 +1,25 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class RecuperoDiagnostico extends BaseModel {
-  public static table = "tbl_recupero_dignosticos"
-  
+  public static table = "tbl_recupero_dignosticos";
+
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
+
+  public serializeExtras() {
+    const keys = Object.keys(this.$extras);
+    const extras = {};
+    keys.forEach((k) => {
+      if (k === "_id") return (extras[k] = this.$extras[k].toString());
+      extras[k] = this.$extras[k];
+    });
+    return extras;
+  }
 }
