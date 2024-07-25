47dd0ac8b3f170a17843923cc8afdb7d07d20617
Bruno Ramari <bjramari@hotmail.com>
2024-07-25 14:41:17 -0300

    creacion de farmacia con usuario asigna perfil al usuario

--- app/Models/FarmaciaAux.ts
+++ app/Models/FarmaciaAux.ts
@@ -4,6 +4,7 @@ import { getConf } from "App/Controllers/Http/ConfigsController";
 import { AccionCRUD, guardarDatosAuditoria } from "App/Helper/funciones";
 import Insertar from "App/Helper/Insertar";
 import Usuario from "./Usuario";
+import UsuarioPerfil from "./UsuarioPerfil";
 
 export default class FarmaciaAux extends Farmacia {
   public static async insertNuevaFarmaciaConUsuario({
@@ -11,7 +12,6 @@ export default class FarmaciaAux extends Farmacia {
   }: {
     ctx: HttpContextContract;
   }) {
-
     const registro = await Insertar._insertarABM({
       ctx,
       formData: ctx.request.body(),
@@ -32,10 +32,12 @@ export default class FarmaciaAux extends Farmacia {
       password: ctx.request.body().password,
     });
     await usuario.save();
+    const asignarPerfil = new UsuarioPerfil();
+    await asignarPerfil.merge({ id_usuario: usuario.id, id_perfil: 2 }).save();
 
     registro.merge({ id_usuario: usuario.id });
 
     await registro.save();
-    return { registroCreado: registro, creado: true }
+    return { registroCreado: registro, creado: true };
   }
 }
